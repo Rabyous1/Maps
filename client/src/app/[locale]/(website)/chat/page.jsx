d933fd15ce21aeaf41d816965c1bff081bb99863
Youssef Raboudi <92892476+Rabyous1@users.noreply.github.com>
2025-09-23 23:32:10 +0100

    Correction build client

--- client/src/app/[locale]/(website)/chat/page.jsx
+++ client/src/app/[locale]/(website)/chat/page.jsx
@@ -1,8 +1,13 @@
 import styles from "@/assets/styles/features/chat/Chat.module.scss";
 import dynamic from 'next/dynamic';
 
-const ChatLayout = dynamic(() => import('@/features/chat/components/ChatLayout'), { ssr: false });
+// Utiliser le composant Messages existant
+const MessagesLayout = dynamic(() => import('@/features/messages/components/MessageThread'), { ssr: false });
 
 export default function Page() {
-  return <ChatLayout styles={styles} />;
+  return (
+    <div className={styles.chatContainer}>
+      <MessagesLayout />
+    </div>
+  );
 }
